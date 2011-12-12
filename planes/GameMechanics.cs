be921854535774a86fc9dd5b07dd306b0b08b126
mikitaby <mikita_by.tut.by>
2011-12-12 12:28:29 +0300

    add IGameMechanics && GameMechanics

---
+++ planes/GameMechanics.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace planes
+{
+    public class GameMechanics : IGameMechanics
+    {
+        public void addPlane()
+        {
+
+        }
+
+        public void drawObjects(Graphics g)
+        {
+
+        }
+
+        public void nextTurn()
+        {
+
+        }
+
+        public void changePlaneParams(Point point, decimal aSpeed, decimal aDegree)
+        {
+
+        }
+    }
+}
---
+++ planes/IGameMechanics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace planes
+{
+    public interface IGameMechanics
+    {
+        void addPlane();
+        void drawObjects(Graphics g);
+        void nextTurn();
+        void changePlaneParams(Point point, decimal aSpeed, decimal aDegree);
+    }
+}
